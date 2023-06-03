964b35f8895ce26c48b32213c84a1b9c56f8c3fd
minsu <naman79@naver.com>
2023-06-03 15:36:40 +0900

    add controller
    add CoreCotroller

---
+++ src/main/java/dev/plattyduckjson/PlattyduckJsonApplication.java
@@ -0,0 +1,13 @@
+package dev.plattyduckjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlattyduckJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlattyduckJsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/plattyduckjson/PlattyduckJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.plattyduckjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlattyduckJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
